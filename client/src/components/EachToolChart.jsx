2369ae0cda85b3cf4ac46808c85c0314953be1ad
Akshaya Thavakumar <122584765+akshayat10@users.noreply.github.com>
2023-08-17 15:15:47 -0400

    Update EachToolChart.jsx

--- client/src/components/EachToolChart.jsx
+++ client/src/components/EachToolChart.jsx
@@ -68,7 +68,9 @@ const closeChartModal = () => {
       });
 
       // Convert data to format of Nivo bar chart
-      const formattedChartData = Object.keys(machineCountsByDate).map((dateString) => {
+      const formattedChartData = Object.keys(machineCountsByDate)
+      .filter((dateString) => dateString !== threeWeeksAgo.toISOString().split("T")[0]) // Exclude the earliest date
+      .map((dateString) => {
         return {
           date: dateString,
           ...machineCountsByDate[dateString],
